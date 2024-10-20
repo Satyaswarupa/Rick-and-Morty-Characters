092d18671718c1fa4f2159cfa318f3bf701d144c
Satyaswarupa parida <127578199+Satyaswarupa@users.noreply.github.com>
2024-10-20 21:47:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,11 @@ const App: React.FC = () => {
 
   return (
     <>
-      {/* Sticky Header */}
       <Header />
-      {/* Main Content (below the header) */}
       <div className="flex flex-grow h-[calc(100vh-64px)] overflow-hidden">
-        {/* Sidebar (Episode List) */}
         <div className="w-1/4 bg-white-800 border-r overflow-y-auto">
           <EpisodeList onSelect={setSelectedEpisodeId} />
         </div>
-
-        {/* Main View (Character List) */}
         <div className="w-3/4 h-full overflow-y-auto">
           <CharacterList episodeId={selectedEpisodeId} />
         </div>
